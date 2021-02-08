bb552cbbc4476072dff5207fc98710ba5a384d0b
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2021-02-08 20:55:50 +0100

    guard peeks into the past

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,11 @@
 
 class PagesController < ApplicationController
   def home
+    date = DateTime.parse params[:start_date]
+    if date < DateTime.current.beginning_of_month
+      params[:start_date] = Date.current.to_s
+    end
+
     @events = Event.all
     @event_sources = EventSource.all
   end
